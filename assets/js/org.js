274483ee7c3f16546e60d263dd40e9d81d073ba1
kladiskov <sandesh2k17@gmail.com>
2018-10-14 11:58:21 +0530

    Remove a newly created entry when clicked on 'X'

--- assets/js/org.js
+++ assets/js/org.js
@@ -1,9 +1,9 @@
 //toggle the text area gray and strike through when clicked
-$("ul").on("click","li",function () {//add a listener to the parent 'ul' so that newly created 'li's will also have the event listeners.
+$("ul").on("click", "li", function () {//add a listener to the parent 'ul' so that newly created 'li's will also have the event listeners.
     $(this).toggleClass("completed");
 });
 
-$("span").click(function (event) {
+$("ul").on("click", "span", function (event) {//add a listener to the parent 'ul' so that newly created span inside 'li's will also have the event listeners.
     $(this).parent().fadeOut(1000, function () {//fade out in 1 sec and then calls remove
         $(this).remove();
     });
@@ -12,8 +12,8 @@ $("span").click(function (event) {
 
 $("input[type='text']").keypress(function (event) {
     if (event.which === 13) {//check if enter key is pressed.
-       var todoText = $(this).val();//get the entered text
-       $("ul").append("<li><span>X </span>" + todoText + "</li>");
+        var todoText = $(this).val();//get the entered text
+        $("ul").append("<li><span>X </span>" + todoText + "</li>");
     }
 });
 
